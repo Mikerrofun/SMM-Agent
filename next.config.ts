5888b95826e41acee2c6133eb51271b467b175df
Mikerrofun <mitja3375@gmail.com>
2026-08-09 16:59:10 +0300

    баг Next.js 16.2.11 с Turbopack при prerendering

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'standalone',
+  
+  experimental: {
+    // @ts-ignore
+    skipTrailingSlashRedirect: true,
+  },
 };
 
 export default nextConfig;

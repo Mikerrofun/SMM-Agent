21e6524f664284c2e68056a6b3c4f642141e8e48
Mikerrofun <mitja3375@gmail.com>
2026-08-09 23:22:08 +0300

    for build after directUrl

--- prisma.config.ts
+++ prisma.config.ts
@@ -3,14 +3,12 @@ import { defineConfig } from "prisma/config";
 
 loadEnv({ path: ".env" });
 
-
 export default defineConfig({
   schema: "prisma/schema.prisma",
   migrations: {
     path: "prisma/migrations",
   },
   datasource: {
-    url: process.env["DIRECT_URL"] ?? process.env["DATABASE_URL"],
+    url: process.env["DIRECT_URL"] ?? process.env["DATABASE_URL"]!,
   },
-
 });

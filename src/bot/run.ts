1849e34a7b485f9fbb085e1659dd5ab57a002bd7
Mikerrofun <mitja3375@gmail.com>
2026-08-09 23:04:30 +0300

    запуск крона первый

--- src/bot/run.ts
+++ src/bot/run.ts
@@ -3,23 +3,19 @@ import { initScheduler } from "../cron";
 
 async function main() {
   try {
-    // Запуск бота
     console.log("🚀 Запуск SMM Agent...\n");
-    
-    await startBot();
-    
     initScheduler();
     
     console.log("\n✅ Все сервисы запущены успешно!\n");
+    await startBot();
+    
   } catch (error) {
     console.error("❌ Ошибка запуска приложения:", error);
     process.exit(1);
   }
 }
 
-
 void main();
-
 
 process.once("SIGINT", () => {
   console.log("\n🛑 Received SIGINT, stopping services...");
